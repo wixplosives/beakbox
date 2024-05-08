0a3b07ec06d9d51f0fffe9cb8fa86735f10998c8
PeterShershov <stelaviv@gmail.com>
2024-05-08 17:27:53 +0300

    arrange icons

--- src/components/player/player.tsx
+++ src/components/player/player.tsx
@@ -1,5 +1,13 @@
 import classNames from 'classnames';
 import styles from './player.module.scss';
+import Heart from '../../assets/heart.svg';
+import Backward from '../../assets/backward.svg';
+import Forward from '../../assets/forward.svg';
+import PauseCircle from '../../assets/pause-circle.svg';
+import PlayCircle from '../../assets/play-circle.svg';
+import RepeateOne from '../../assets/repeate-one.svg';
+import VolumeHigh from '../../assets/volume-high.svg';
+import Shuffle from '../../assets/shuffle.svg';
 
 export interface PlayerProps {
     className?: string;
@@ -10,5 +18,16 @@ export interface PlayerProps {
  * To create custom component templates, see https://help.codux.com/kb/en/article/kb16522
  */
 export const Player = ({ className }: PlayerProps) => {
-    return <div className={classNames(styles.root, className)}></div>;
+    return (
+        <div className={classNames(styles.root, className)}>
+            <Heart />
+            <Shuffle />
+            <Backward />
+            <PlayCircle />
+            <PauseCircle />
+            <Forward />
+            <RepeateOne />
+            <VolumeHigh />
+        </div>
+    );
 };
