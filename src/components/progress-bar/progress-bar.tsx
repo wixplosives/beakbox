1a49cfe4bae799da5b674ff5ab4557a7b69e2b97
PeterShershov <stelaviv@gmail.com>
2024-05-09 16:17:59 +0300

    remove comment from progress bar

--- src/components/progress-bar/progress-bar.tsx
+++ src/components/progress-bar/progress-bar.tsx
@@ -6,14 +6,10 @@ export interface ProgressBarProps {
     progress: number; // 0-100
 }
 
-/**
- * This component was created using Codux's Default new component template.
- * To create custom component templates, see https://help.codux.com/kb/en/article/kb16522
- */
 export const ProgressBar = ({ className, progress }: ProgressBarProps) => {
     return (
         <div className={classNames(styles.root, className)}>
-            <div className={styles.inner} style={{width: `${progress}%`}} />
+            <div className={styles.inner} style={{ width: `${progress}%` }} />
         </div>
     );
 };
