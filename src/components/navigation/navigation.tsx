f7940585296b90f6ec341a96441a0e1cb01625b3
Itay Ben Porat <itayp@wix.com>
2024-05-15 16:00:02 +0300

    Fix PR comments

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -7,7 +7,6 @@ import NavigationRadioIcon from '../../assets/navigation-radio-icon.svg?react';
 import NavigationAlbumsIcon from '../../assets/navigation-albums-icon.svg?react';
 import NavigationPodcastsIcon from '../../assets/navigation-podcasts-icon.svg?react';
 import { NavItem } from '../nav-item/nav-item';
-import NavItem_module from '../nav-item/nav-item.module.scss';
 import NavigationRecentlyIcon from '../../assets/navigation-recently-icon.svg?react';
 import NavigationFavoriteIcon from '../../assets/navigation-favorite-icon.svg?react';
 import NavigationLocalIcon from '../../assets/navigation-local-icon.svg?react';
@@ -24,52 +23,44 @@ export const Navigation = ({ className }: navigationProps) => {
             <div className={styles.logoContainer}>
                 <Beakboxlogo className={styles.beakBoxLogo} />
             </div>
-            <div>
+            <ul className={styles.menu}>
                 <NavItem
                     title={'Home'}
                     icon={<NavigationHomeIcon className={styles.navigationIcon} />}
-                    className={NavItem_module.listItems}
                 />
                 <NavItem
                     title={'Discover'}
                     icon={<NavigationDiscoverIcon className={styles.navigationIcon} />}
-                    className={NavItem_module.listItems}
                 />
                 <NavItem
                     title={'Radio'}
                     icon={<NavigationRadioIcon className={styles.navigationIcon} />}
-                    className={NavItem_module.listItems}
                 />
                 <NavItem
                     title={'Albums'}
                     icon={<NavigationAlbumsIcon className={styles.navigationIcon} />}
-                    className={NavItem_module.listItems}
                 />
                 <NavItem
                     title={'Podcasts'}
                     icon={<NavigationPodcastsIcon className={styles.navigationIcon} />}
-                    className={NavItem_module.listItems}
                 />
-            </div>
-            <div>
+            </ul>
+            <ul className={styles.menu}>
                 <div className={styles.menuTitle}>Library</div>
                 <NavItem
                     title={'Recently Added'}
                     icon={<NavigationRecentlyIcon className={styles.navigationIcon} />}
-                    className={NavItem_module.listItems}
                 />
                 <NavItem
                     title={'Favorite Songs'}
                     icon={<NavigationFavoriteIcon className={styles.navigationIcon} />}
-                    className={NavItem_module.listItems}
                 />
                 <NavItem
                     title={'Local Files'}
                     icon={<NavigationLocalIcon className={styles.navigationIcon} />}
-                    className={NavItem_module.listItems}
                 />
-            </div>
-            <div>
+            </ul>
+            <ul className={styles.menu}>
                 <div className={styles.menuTitle}>
                     Playlist
                     <button className={styles.addPlaylistButton}>
@@ -79,19 +70,16 @@ export const Navigation = ({ className }: navigationProps) => {
                 <NavItem
                     title={'Lo-fi Music'}
                     icon={<NavigationPlaylistIcon className={styles.navigationIcon} />}
-                    className={NavItem_module.listItems}
                 />
                 <NavItem
                     title={'Best of Bon Jovi'}
                     icon={<NavigationPlaylistIcon className={styles.navigationIcon} />}
-                    className={NavItem_module.listItems}
                 />
                 <NavItem
                     title={'Best of FKJ'}
                     icon={<NavigationPlaylistIcon className={styles.navigationIcon} />}
-                    className={NavItem_module.listItems}
                 />
-            </div>
+            </ul>
         </div>
     );
 };
