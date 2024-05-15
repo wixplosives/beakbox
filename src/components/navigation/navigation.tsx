76f57f0524a0fd05c02487046162e196057cc0e4
Itay Ben Porat <itayp@wix.com>
2024-05-15 14:34:38 +0300

    Revert "Wow OMG"
    This reverts commit bd9debcfe6d36c0e7270f54701e415420c1d7a51.

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -1,6 +1,6 @@
 import classNames from 'classnames';
 import styles from './navigation.module.scss';
-import Beakboxlogo from '../../assets/beakboxlogo.svg?react';
+import BeakboxlogoSvg from '../../assets/beakboxlogo.svg?react';
 import NavigationHomeIcon from '../../assets/navigation-home-icon.svg?react';
 import NavigationDiscoverIcon from '../../assets/navigation-discover-icon.svg?react';
 import NavigationRadioIcon from '../../assets/navigation-radio-icon.svg?react';
@@ -8,11 +8,6 @@ import NavigationAlbumsIcon from '../../assets/navigation-albums-icon.svg?react'
 import NavigationPodcastsIcon from '../../assets/navigation-podcasts-icon.svg?react';
 import { NavItem } from '../nav-item/nav-item';
 import NavItem_module from '../nav-item/nav-item.module.scss';
-import NavigationRecentlyIcon from '../../assets/navigation-recently-icon.svg?react';
-import NavigationFavoriteIcon from '../../assets/navigation-favorite-icon.svg?react';
-import NavigationLocalIcon from '../../assets/navigation-local-icon.svg?react';
-import NavigationAddplaylistIcon from '../../assets/navigation-addplaylist-icon.svg?react';
-import NavigationPlaylistIcon from '../../assets/navigation-playlist-icon.svg?react';
 
 export interface navigationProps {
     className?: string;
@@ -22,73 +17,32 @@ export const Navigation = ({ className }: navigationProps) => {
     return (
         <div className={classNames(styles.root, className, styles.navigation)}>
             <div className={styles.logoContainer}>
-                <Beakboxlogo className={styles.beakBoxLogo} />
+                <BeakboxlogoSvg className={styles.beakBoxLogo} />
             </div>
             <div>
                 <NavItem
                     title={'Home'}
-                    icon={<NavigationHomeIcon className={styles.navigationIcon} />}
+                    icon={<NavigationHomeIcon className={NavItem_module.navigationIcon} />}
                     className={NavItem_module.listItems}
                 />
                 <NavItem
                     title={'Discover'}
-                    icon={<NavigationDiscoverIcon className={styles.navigationIcon} />}
+                    icon={<NavigationDiscoverIcon className={NavItem_module.navigationIcon} />}
                     className={NavItem_module.listItems}
                 />
                 <NavItem
                     title={'Radio'}
-                    icon={<NavigationRadioIcon className={styles.navigationIcon} />}
+                    icon={<NavigationRadioIcon className={NavItem_module.navigationIcon} />}
                     className={NavItem_module.listItems}
                 />
                 <NavItem
                     title={'Albums'}
-                    icon={<NavigationAlbumsIcon className={styles.navigationIcon} />}
+                    icon={<NavigationAlbumsIcon className={NavItem_module.navigationIcon} />}
                     className={NavItem_module.listItems}
                 />
                 <NavItem
                     title={'Podcasts'}
-                    icon={<NavigationPodcastsIcon className={styles.navigationIcon} />}
-                    className={NavItem_module.listItems}
-                />
-            </div>
-            <div>
-                <div className={styles.menuTitle}>Library</div>
-                <NavItem
-                    title={'Recently Added'}
-                    icon={<NavigationRecentlyIcon className={styles.navigationIcon} />}
-                    className={NavItem_module.listItems}
-                />
-                <NavItem
-                    title={'Favorite Songs'}
-                    icon={<NavigationFavoriteIcon className={styles.navigationIcon} />}
-                    className={NavItem_module.listItems}
-                />
-                <NavItem
-                    title={'Local Files'}
-                    icon={<NavigationLocalIcon className={styles.navigationIcon} />}
-                    className={NavItem_module.listItems}
-                />
-            </div>
-            <div>
-                <div className={styles.menuTitle}>
-                    Playlist
-                    <button className={styles.addPlaylistButton}>
-                        <NavigationAddplaylistIcon />
-                    </button>
-                </div>
-                <NavItem
-                    title={'Lo-fi Music'}
-                    icon={<NavigationPlaylistIcon className={styles.navigationIcon} />}
-                    className={NavItem_module.listItems}
-                />
-                <NavItem
-                    title={'Best of Bon Jovi'}
-                    icon={<NavigationPlaylistIcon className={styles.navigationIcon} />}
-                    className={NavItem_module.listItems}
-                />
-                <NavItem
-                    title={'Best of FKJ'}
-                    icon={<NavigationPlaylistIcon className={styles.navigationIcon} />}
+                    icon={<NavigationPodcastsIcon className={NavItem_module.navigationIcon} />}
                     className={NavItem_module.listItems}
                 />
             </div>
