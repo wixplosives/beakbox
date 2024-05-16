6de916bf67dca2603bf7185a9b89e45ada591375
Itay Ben Porat <itayp@wix.com>
2024-05-16 18:36:38 +0300

    Add cards to Popular Artists

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,23 @@ import { TopBar } from './components/top-bar/top-bar';
 import { useState } from 'react';
 import { AppContext } from './components/app-context';
 import { NAVIGATION_SECTIONS, SONGS } from './constants';
+import RapperDuck from './assets/sample-images/rapper-duck.webp';
+import ChillDuck from './assets/sample-images/chill-duck.webp';
+import EighteesDuck from './assets/sample-images/eightees-duck.webp';
+import DjDuck from './assets/sample-images/dj-duck.webp';
+import JazzDuck from './assets/sample-images/jazz-duck.webp';
+import CoupleDuck from './assets/sample-images/couple-duck.webp';
+import { PopularReleases } from './components/popular-releases/popular-releases';
 
+const popularReleasesSamples = [
+    { imageUrl: ChillDuck, title: 'Anat Marley', subTitleOne: '2420', subTitleTwo: 'Song' },
+    { imageUrl: RapperDuck, title: 'Pete-D', subTitleOne: '2012', subTitleTwo: 'Song' },
+    { imageUrl: CoupleDuck, title: 'Itay & Ben', subTitleOne: '2069', subTitleTwo: 'Album' },
+    { imageUrl: JazzDuck, title: 'Alexey Davis', subTitleOne: '2017', subTitleTwo: 'Album' },
+    { imageUrl: DjDuck, title: 'Omry Smaduck', subTitleOne: '2020', subTitleTwo: 'Song' },
+    { imageUrl: ChillDuck, title: 'ZagZag', subTitleOne: '2023', subTitleTwo: 'Song' },
+    { imageUrl: EighteesDuck, title: 'Mcducky', subTitleOne: '2020', subTitleTwo: 'Album' },
+];
 function App() {
     const [selectedSection, selectSection] = useState('Home');
 
@@ -22,6 +38,7 @@ function App() {
                         playing={SONGS[3]}
                         className={styles.playlist}
                     />
+                    <PopularReleases popularData={popularReleasesSamples} />
                 </main>
                 <Player className={styles.player} />
             </div>
