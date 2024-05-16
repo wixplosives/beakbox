f7a1dbcb0f9c7b2a8a63fe3efe93512d7003fda0
Itay Ben Porat <itayp@wix.com>
2024-05-16 18:44:47 +0300

    Add image to the Card board

--- src/_codux/boards/card/card.board.tsx
+++ src/_codux/boards/card/card.board.tsx
@@ -1,9 +1,12 @@
 import { createBoard } from '@wixc3/react-board';
 import { Card } from '../../../components/card/card';
+import RapperDuckWebp from '../../../assets/sample-images/rapper-duck.webp';
 
 export default createBoard({
     name: 'Card',
-    Board: () => <Card subTitleOne="2024" subTitleTwo="Album" title="Mcducky" />,
+    Board: () => (
+        <Card subTitleOne="2024" subTitleTwo="Album" title="Mcducky" imageUrl={RapperDuckWebp} />
+    ),
     isSnippet: true,
     environmentProps: {
         canvasHeight: 321,
