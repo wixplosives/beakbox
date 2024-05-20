6859cbed8caceaf7172a785e359447fc10cb08d2
Alexey Lebedev <alisey.login@gmail.com>
2024-05-20 21:26:16 +0300

    Add account menu

--- src/components/right-sidebar/right-sidebar.tsx
+++ src/components/right-sidebar/right-sidebar.tsx
@@ -5,6 +5,7 @@ import { RecentlyPlayedItem } from '../recently-played-item/recently-played-item
 import { RecentlyPlayedList } from '../recently-played-list/recently-played-list';
 import styles from './right-sidebar.module.scss';
 import { timeAgo } from '../time-ago';
+import { AccountMenu } from '../account-menu/account-menu';
 
 export interface RightSidebarProps {
     className?: string;
@@ -13,6 +14,11 @@ export interface RightSidebarProps {
 export const RightSidebar = ({ className }: RightSidebarProps) => {
     return (
         <div className={classNames(styles.root, className)}>
+            <AccountMenu
+                userName="James Newell Osterberg"
+                userImage="/src/assets/iggy/iggy.jpg"
+                isPremium={true}
+            />
             <FansAlsoLike items={songs.slice(0, 4)} />
             <RecentlyPlayedList>
                 {songs.map((item, i) => (
